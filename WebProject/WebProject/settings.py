c8c65c463e64bedb0fa4b5cb908b388e704e710b
WaqasDogar <85635866+WaqasDogar@users.noreply.github.com>
2022-07-22 22:39:51 +0500

    Update settings.py

--- WebProject/WebProject/settings.py
+++ WebProject/WebProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hgajpq9i$qcbb9+7a5@!i9m8(05bk5!ee@stp%a3(z!a3g9ya0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
